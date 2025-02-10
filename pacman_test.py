3eafb256834ed5c6ccb7d47657fe4b61cbab8e0f
jposada2020 <jquintana202020@gmail.com>
2025-02-09 21:35:19 -0500

    upd

--- pacman_test.py
+++ pacman_test.py
@@ -9,8 +9,8 @@ from machine import Pin
 # Create a NeoPixel strip with 8 pixels connected to pin 15
 led_strip = NEOPIXEL(Pin(15), 50)
 
-
+# Fill the strip with black (0, 0, 0) to start
 led_strip.fill_all(color=(0, 0, 0))
+
+# Pacman animation
 led_strip.pacman(30)
-
-
